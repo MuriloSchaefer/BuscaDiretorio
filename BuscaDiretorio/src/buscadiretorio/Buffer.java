ca4cea30ad1f8ab33b1b1a8b04570892d008203d
MuriloSchaefer <murilo.schaefer@gmail.com>
2016-07-09 12:39:05 -0300

    nao sei, o que mudou, mas nada esta igual

--- BuscaDiretorio/src/buscadiretorio/Buffer.java
+++ BuscaDiretorio/src/buscadiretorio/Buffer.java
@@ -1,9 +1,11 @@
+package buscadiretorio;
+
 /*
  * To change this license header, choose License Headers in Project Properties.
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
-package buscadiretorio;
+
 
 import java.io.File;
 import java.util.ArrayList;
